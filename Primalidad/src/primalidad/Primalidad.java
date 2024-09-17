9a01bec98cb507790069c28a220c2ca0b75b0162
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-17 10:43:58 +0200

    ejercicio de numeros primos

--- Primalidad/src/primalidad/Primalidad.java
+++ Primalidad/src/primalidad/Primalidad.java
@@ -1,20 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package primalidad;
 
-/**
- *
- * @author User
- */
+import java.util.Scanner;
+
+
 public class Primalidad {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int numero = teclado.nextInt();
+        if(esPrimo(numero)){
+            System.out.println("Es un numero primo");
+        }else{
+            System.out.println("No es un numero primo");
+        }
+        
     }
-    
+    static boolean esPrimo(int number){
+        int contador = 0;
+        int []numeros= new int [number];
+        for(int i =0; i<numeros.length;i++ ){
+            numeros[i]= i+1;
+        }
+        for(int i:numeros){
+            if(i==1|| i== number){
+                continue;
+            }
+            if(number%i ==0){
+               contador++; 
+            }
+        }
+        return contador==0;
+    }
 }
